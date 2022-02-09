509d6ab643f3ffc2dd534fe31707bb2a8f7e2149
tomnoble20 <tomnoble1904@gmail.com>
2022-02-09 21:38:20 +0000

    feat:created static global header with search bar and action buttons

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
+let cssModulesIndex = 0;
+const cssModulesIndexes = {};
+const getLocalIdent = (context, _, localName) => {
+  const id = context.resourcePath + localName;
+  const index = (cssModulesIndexes[id] = cssModulesIndexes[id] || cssModulesIndex++);
+  return `--${index}`;
+};
 module.exports = {
   reactStrictMode: true,
-}
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              getLocalIdent
+            },
+          },
+        ],
+      }
+    ]
+  }
+};
+
