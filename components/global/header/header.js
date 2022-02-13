e927d92b464b5b1976d8fe6c197bf1a842116578
Tom Noble <tomnoble1904@gmail.com>
2022-02-13 21:34:31 +0000

    feat:added basic modal component

--- components/global/header/header.js
+++ components/global/header/header.js
@@ -3,14 +3,14 @@ import SearchBar from '../search-bar/search-bar'
 import HeaderActions from '../header-actions/header-actions'
 import styles from './header.module.css'
 
-export default function GlobalHeader(){
+export default function GlobalHeader({listenForModalTrigger}){
     return(
     <header className={styles.globalHeader}>
         <div className={styles['global-header__logo']}>
             <Image src="/logo.svg" alt="Ribbit Logo" width={32} height={32} />
         </div>
         <SearchBar></SearchBar>
-        <HeaderActions></HeaderActions>
+        <HeaderActions listenForModalTrigger={listenForModalTrigger}></HeaderActions>
     </header>
     )
 }
---
+++ components/global/modal/modal.js
@@ -0,0 +1,23 @@
+import styles from './modal.module.css';
+
+export default function Modal({isModalActive, modalTrigger, children}) {
+    function handleClick() {
+        modalTrigger(setModal => false)
+    }
+
+    return(
+        <div className={isModalActive ? `${styles['modal']} ${styles['modal--active']}` : `${styles['modal']}`} role="dialog" aria-modal="true">
+            <div className={styles['modal__overlay']}></div>
+            <div className={styles['modal__inner']}>
+                <header className={styles['modal__header']}>
+                    <button className={styles['modal__close-trigger']} type="button" onClick={handleClick}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button> 
+                </header>
+                <div className={styles['modal__body']}>{children}</div> 
+            </div>
+        </div>
+    )
+}
